e9b150fef42470b198c714a809938064b6f6cd48
Goverdhan T M <goverdhanmadhavarao@gmail.com>
2026-04-28 08:38:14 +0530

    Use same-origin API base for deployed frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = window.location.origin;
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const PRIORITY_OPTIONS = [
     { value: "days_off", label: "Days Off" },
